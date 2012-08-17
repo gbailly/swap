248b705ce5ccf4995ae674dc3b94acac4de46ae3
Guillaume <guillaume@guillaumes-macbook-pro.local>
2012-08-17 19:54:25 +0200

    Set up base dir in Locations

--- swap.js
+++ swap.js
@@ -41,9 +41,9 @@ fileLocation = Locations.BASE_DIR + '/';
 
 // Routes
 
-app.get('/', function(req, res) {
-  res.render('setup', { step: null, layout: 'layout_protocols' });
-});
+require('./server/home.js');
+require('./server/setup.js');
+require('./server/utils.js');
 
 
 var port = process.env.PORT || 5000;
