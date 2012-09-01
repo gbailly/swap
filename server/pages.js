58c9ce733a568e775c1e73a35880ec9b53f5b4fd
gbailly <guillaume.gbailly@gmail.com>
2012-09-01 15:39:23 +0200

    Clean repository

--- server/pages.js
+++
@@ -1,9 +0,0 @@
-// home page
-app.get('/', function(req, res) {
-  res.render('home', { layout: 'layout' });
-});
-
-// page describing functionalities
-app.get('/functionalities', function(req, res) {
-  res.render('functionalities', { layout: 'layout' });
-});
